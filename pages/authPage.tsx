3b36ca7ac309c0cee4c0cb8a01bd13b27329d6d6
Wilfred Osozi <osoziw@gmail.com>
2025-09-27 00:15:48 +0300

    Add authentication page for sign in/sign up

---
+++ pages/authPage.tsx
@@ -0,0 +1,61 @@
+import SignIn from '@/components/common/signin';
+import SignUp from '@/components/common/signup';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useAuth } from '@/firebase/auth';
+
+type Authmode = "signin" | "signup"
+
+const AuthPage: React.FC = () => {
+    const [mode, setMode]= useState<Authmode>("signin")
+    const router = useRouter();
+    const {user, loading}= useAuth();
+
+   useEffect(()=>{
+     if(!loading && user){
+        const redirect= (router.query.redirect as string) || "/";
+        router.push(redirect)
+    }
+   },[user, router])
+
+
+
+    return (
+        <div className='flex flex-col items-center justify-center min-h-screen bg-gray-50 mt-10'>
+           <div className='w-full max-w-md rounded-2xl bg-white p-6 shadow-lg'>
+             <h1 className='text-center text-2xl font-bold text-gray-800 mb-4'>
+                {mode==="signin"? "Sign In": "Sign Up"} 
+            </h1>
+                
+                {mode === "signin"? <SignIn/> : <SignUp/>}
+
+            <button onClick={()=> setMode(mode==="signin"? "signup": "signin")}>
+
+                <p className='mt-6 text-center text-sm text-gray-600'>
+                    {mode==="signin"? (
+                        <>
+                        Don't have an account?{""} 
+                        <span onClick={()=>setMode("signup")}
+                        className='cursor-pointer font-semibold text-yellow-600 hover:underline'>
+                         Create one 
+                        </span>
+                        </>
+                    ):(
+                        <>
+                        Already have an account?{""}
+                        <span onClick={()=>setMode("signin")} 
+                        className='cursor-pointer font-semibold text-yellow-600 hover:underline'>
+                            Sign In
+                        </span>
+                        </>
+                    )}     
+                </p>
+
+            </button>
+           </div>
+      
+        </div>
+    );
+};
+
+export default AuthPage;
