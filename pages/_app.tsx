240e8d276ea08e140c3f0fb414d7e5c20fe13729
Wilfred Osozi <osoziw@gmail.com>
2025-09-27 00:13:01 +0300

    Set up custom App component in Next.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
+import { CartProvider } from "@/context/CartContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Header from "@/components/layouts/header";
+import { AuthProvider } from "firebase/auth";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return(
+      <CartProvider>
+      <Header/>
+       <Component {...pageProps} />
+    </CartProvider>
+   
+  );
 }
