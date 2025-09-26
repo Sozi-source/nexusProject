1031cca4747c2f25561938e78591e8e05526a21c
Wilfred Osozi <osoziw@gmail.com>
2025-09-27 00:15:07 +0300

    Add TypeScript interfaces and types

---
+++ interfaces/index.ts
@@ -0,0 +1,57 @@
+// /interfaces/index.ts
+
+export interface Review {
+  rating: number
+  comment: string
+  date: string
+  reviewerName: string
+  reviewerEmail: string
+}
+
+export interface Dimensions {
+  width: number
+  height: number
+  depth: number
+}
+
+export interface Meta {
+  createdAt: string
+  updatedAt: string
+  barcode: string
+  qrCode: string
+}
+
+export interface productsProps {
+  id: number
+  title: string
+  description: string
+  category: string
+  price: number
+  discountPercentage: number
+  rating: number
+  stock: number
+  quantity: number
+  tags: string[]
+  brand: string
+  sku: string
+  weight: number
+  dimensions: Dimensions
+  warrantyInformation: string
+  shippingInformation: string
+  availabilityStatus: string
+  reviews: Review[]
+  returnPolicy: string
+  minimumOrderQuantity: number
+  meta: Meta
+  images: string[]
+  thumbnail: string
+}
+
+export interface CartContextprops{
+  cart: productsProps[];
+  addToCart: (item:productsProps)=>void;
+  removeFromCart: (id:number)=>void;
+  incrementQuantity: (id:number)=>void;
+  decrementQuantity: (id:number)=>void;
+  getTotalPrice:()=> number; 
+}
