37817340a8edd2802f6c4773f5f15a82cf3c4eda
Wilfred Osozi <osoziw@gmail.com>
2025-09-27 00:14:28 +0300

    Add React context for global state management

---
+++ context/CartContext.tsx
@@ -0,0 +1,63 @@
+import { CartContextprops, productsProps } from "@/interfaces";
+import { createContext, ReactNode, useContext, useState } from "react";
+
+
+export const CartContext= createContext<CartContextprops|null>(null)
+
+export const useCart =()=>{
+    const context= useContext(CartContext)
+    if(!context) throw new Error("Wrap the components inside the CartProvider")
+        return context;
+}
+
+export const CartProvider=({children}:{children:ReactNode})=>{
+    const[cart, setCart]= useState<productsProps[]>([])
+
+    // add ti cart
+    const addToCart=(item:productsProps)=>{
+       setCart((prev)=>{
+        const exist = prev.find((cartItem)=> cartItem.id === item.id)
+        if(exist) return prev;
+        return [...prev, {...item, quantity:1}]
+       })
+}
+
+    // remove from cart
+    const removeFromCart =(id:number)=>{
+        setCart((prev)=>prev.filter((product)=>product.id !==id))
+    }
+
+    // increment product quantity
+    const incrementQuantity =(id:number)=>{
+        setCart((prev)=> 
+            prev.map((cartItem)=>
+                cartItem.id===id? {...cartItem, quantity:(cartItem.quantity||1)+1}:cartItem))
+    }
+
+    // decrement product quantity
+     const decrementQuantity =(id:number)=>{
+        setCart((prev)=> 
+            prev.map((cartItem)=>
+                cartItem.id===id? {...cartItem, quantity:(cartItem.quantity||1)-1}
+        :cartItem
+    )
+    .filter((cartItem) => (cartItem.quantity??0) > 0)
+    )
+    
+    }
+
+    const getTotalPrice = () => {
+  return cart.reduce((total, item) => {
+    return total + (item.price * (item.quantity ?? 1));
+  }, 0);
+};
+
+return(
+    <CartContext.Provider value={{cart, addToCart, removeFromCart, incrementQuantity, decrementQuantity, getTotalPrice}}>
+        {children}
+    </CartContext.Provider>
+)
+}
+
+
+
