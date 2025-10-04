ecad714f997c1b0e9d83af2a34fa81abcc0ba712
Wilfred Osozi <osoziw@gmail.com>
2025-10-04 18:38:10 +0300

    modified the manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,9 +9,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return(
       <CartProvider>
       <Head>
-        <link rel="manifest" href="/manifest.json" />
-         <meta name="theme-color" content="#f59e0b" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" type="image/png" sizes="192x192" href="/assets/images/icon-192x192.png" />
+          <link rel="apple-touch-icon" href="/assets/images/icon-512x512.png" />
+          <meta name="theme-color" content="#f59e0b" />
       </Head>
+
       <Header/>
        <Component {...pageProps} />
        <Footer/>
