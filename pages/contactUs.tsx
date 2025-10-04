266e5bb0674573d7303ce66298e511d352c193cb
Wilfred Osozi <osoziw@gmail.com>
2025-10-04 15:39:34 +0300

    added map location iframe

--- pages/contactUs.tsx
+++ pages/contactUs.tsx
@@ -33,42 +33,76 @@ const ContactUs:React.FC=()=>{
     };
 
     return(
-        <div className="grid grid-cols-3 mt-[10%] bg-white">
-        <div className="flex flex-col items-start justify-start space-y-4 px-4 shadow rounded-md m-5 p-5 hover:bg-gray-100">
+        <div className="min-h-screen bg-gray-100 py-20 px-4 sm:px-6 lg:px-8 mt-[5%]">
+            <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-3 gap-6">
+
+                {/* contact information */}
+        <div className="flex flex-col items-start justify-start space-y-4 px-4 py-6 shadow rounded-md hover:bg-gray-100 bg-white">
             <h3 className="text-lg font-bold text-yellow-500 font-serif">Contact us</h3>
-            <p className="flex items-center space-x-3 hover:text-blue-600 gap-3 "><MdEmail className="h-8 w-6"/> <span>shop@eduka.co.ke</span></p>
-            <p className="flex items-center space-x-3 hover:text-blue-600 gap-3"><PhoneCallIcon className="h-8 w-6"/><span> +254711390861</span></p>
+            <p className="flex items-center space-x-3 hover:text-blue-600 gap-3 "><MdEmail className="h-6 w-6 text-gray-700"/> <span>shop@eduka.co.ke</span></p>
+            <p className="flex items-center space-x-3 hover:text-blue-600 gap-3"><PhoneCallIcon className="h-6 w-6 text-gray-700"/><span> +254711390861</span></p>
             <p className="flex items-center space-x-3 gap-3"><CiLocationOn className="h-6 w-6 text-gray-600"/><span>Nairobi, Kenya</span> </p>
         </div>
 
-        <div className="flex flex-col shadow rounded-md mb-[10%]">
-            <form onSubmit={handleFormSubmit} className="border border-gray-100 rounded-md p-5">
+            {/* Message form */}
+        <div className="flex flex-col shadow rounded-md bg-white p-6">
+            <form onSubmit={handleFormSubmit} className="flex flex-col space-y-4">
                 <h3 className="text-lg font-bold text-yellow-500 font-serif">Message Us</h3>
+
+                <div className="flex flex-col">
                 <label htmlFor="name" className="mt-5">Name</label>
                 <input 
-                type="text" 
-                value={name}
-                onChange={(e)=>setName(e.target.value)}
-                placeholder="Enter your full name"
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"/>
+                    type="text" 
+                    value={name}
+                    onChange={(e)=>setName(e.target.value)}
+                    placeholder="Enter your full name"
+                    className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"/>
 
+                </div>
+                
+                <div>
                 <label htmlFor="email">Email</label>
                 <input type="email"
-                value={email} 
-                onChange={(e)=>setEmail(e.target.value)}
-                placeholder="Enter your email"
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"/>
+                    value={email} 
+                    onChange={(e)=>setEmail(e.target.value)}
+                    placeholder="Enter your email"
+                    className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"/>
+                </div>
 
+                <div>
                 <label htmlFor="message">Message</label>
                 <textarea name="message"
-                value={message}
-                onChange={(e)=>setMessage(e.target.value)}
-                placeholder="Your message"
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500">
+                    value={message}
+                    onChange={(e)=>setMessage(e.target.value)}
+                    placeholder="Your message"
+                    className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500">
                 </textarea>
+                </div>
                 <button type="submit" className="w-full bg-blue-600 text-white font-semibold py-2 rounded-lg hover:bg-blue-700 transition-colors">Submit</button>
             </form>
         </div>
+
+        {/* Map location */}
+        <div className="px-4 sm:px-6 lg:px-8 shadow bg-white">
+            <h3 className="text-lg sm:text-xl font-semibold text-yellow-500 mb-4 text-center ">Find us On the Map</h3>
+            <div className="w-full h-32 sm:h-40 md:h-48 lg:h-[250px] xl:h-[300px] rounded-md overflow-hidden shadow-md mt-6">
+                <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d127649.13044240138!2d37.00207473267293!3d-1.1351346871630512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2ske!4v1759580590147!5m2!1sen!2ske"
+                className="w-full h-full border-0"
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+            </div>
+        </div>
+
+
+            </div>
+
+
+       
+
+        
         </div>
     )
 }
