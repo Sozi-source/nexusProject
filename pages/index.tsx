232e10f7415c05aef5e6c8340acfdaed4795a57b
Wilfred Osozi <osoziw@gmail.com>
2025-10-04 16:51:40 +0300

    addedresponsive style on contact page

--- pages/index.tsx
+++ pages/index.tsx
@@ -139,7 +139,7 @@ const Home: React.FC = () => {
         </div>
 
         {/* Products Grid */}
-        <h2 className="text-2xl font-bold mb-4">
+        <h2 className="text-2xl font-bold mb-4 text-gray-800">
           {selectedCategory
             ? `Products in ${selectedCategory.replace("-", " ")}`
             : "All Products"}
