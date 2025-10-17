afb20489b9dd2a3ef7c2b8b73cd7147972f12332
Wilfred Osozi <osoziw@gmail.com>
2025-10-17 12:40:36 +0300

    updated the product card

--- components/common/ProductCard.tsx
+++ components/common/ProductCard.tsx
@@ -21,22 +21,22 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
     <div onClick={handleClick}
     className="rounded-xl overflow-hidden bg-white hover:shadow-lg hover:scale-[1.01] transition-all duration-200 flex flex-col h-full hover:bg-gray-100 shadow">
       {/* Responsive Image Section */}
-      <div className="relative aspect-[4/3] w-full overflow-hidden">
+      <div className="relative w-full h-40 sm:h-56 md:h-60 lg:h-64 px-4 py-3 sm:px-0 sm:py-0">
         <Image
           src={product.thumbnail}
           alt={product.title}
           fill
-          className="object-cover transition-transform duration-300 hover:scale-105"
-          sizes="(max-width: 640px) 100vw,
-                (max-width: 768px) 50vw,
-                (max-width: 1024px) 33vw,
-                25vw"
+          width={0}
+          height={0}
+          className="object-cover"
+          sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
+          priority
         />
-</div>
+      </div>
 
       {/* Text Section */}
       <div className="p-3 sm:p-4 flex-1">
-        <h3 className="text-black sm:text-lg font-serif mb-1 line-clamp-1">
+        <h3 className="text-black sm:text-lg font-semibold mb-1 line-clamp-1">
           {product.title}
         </h3>
         <p className="text-sm sm:text-base text-gray-800 font-medium mb-1">
